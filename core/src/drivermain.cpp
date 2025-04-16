fb74e141810f181085c7f1428edb28c70b09732d
Charlie Brown <piratebooty309@gmail.com>
2025-04-16 06:54:39 -0700

    Delete core directory

--- core/src/drivermain.cpp
+++
@@ -1,7 +0,0 @@
-#include <ntifs.h>
-#include <wdm.h>
-#include "dcdriver.hpp"
-
-extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
-    return DiskCryptor::DriverMain::Instance().Initialize(DriverObject, RegistryPath);
-}
--- core/src/encryptor.cpp
+++
@@ -1,1 +0,0 @@
-# left this empty for security and legal reasons. Use your imagination :P
