00fbad217229421139e63e7e2f1aca134bc15f65
Charlie Brown <piratebooty309@gmail.com>
2025-04-15 19:54:17 -0700

    Update drivermain.cpp

--- core/src/drivermain.cpp
+++ core/src/drivermain.cpp
@@ -1,6 +1,6 @@
 #include <ntifs.h>
 #include <wdm.h>
-#include "DCDriver.hpp"
+#include "dcdriver.hpp"
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
     return DiskCryptor::DriverMain::Instance().Initialize(DriverObject, RegistryPath);
